32ce5cd68d2296266070bde15fdbf9bc348af179
Yeo Si Zhao <sizhaoyeo1@gmail.com>
2026-01-20 22:11:07 +0800

    Gave Chatbot new name + greetings

---
+++ src/main/java/SixSeven.java
@@ -0,0 +1,9 @@
+public class SixSeven {
+    public static void main(String[] args) {
+        String name = "SixSeven";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
