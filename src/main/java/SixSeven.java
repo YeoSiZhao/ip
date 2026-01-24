46657e209bc9192500342b2b1d5dacc9d121e5a0
Yeo Si Zhao <sizhaoyeo1@gmail.com>
2026-01-24 21:32:26 +0800

    Added echos commands entered by the user

--- src/main/java/SixSeven.java
+++ src/main/java/SixSeven.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class SixSeven {
     public static void main(String[] args) {
         String name = "SixSeven";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        if (line.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(line);
+        }
     }
 }
 
