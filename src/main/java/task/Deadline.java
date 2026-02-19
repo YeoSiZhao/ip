1985cd12159346fa47a566bf1762ffd234a9ae56
Yeo Si Zhao <sizhaoyeo1@gmail.com>
2026-02-20 01:59:18 +0800

    Add space for deadline

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -14,6 +14,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return String.format("[D][%s] %s(by: %s)", markString(), description, by);
+        return String.format("[D][%s] %s (by: %s)", markString(), description, by);
     }
 }
